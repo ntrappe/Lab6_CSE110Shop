bc8cee3fa58506be944bf66099224ce70ef7b54e
ntrappe <33298966+ntrappe@users.noreply.github.com>
2021-02-13 22:38:58 -0800

    update CSS w max-height for bracelet

--- components/product-item.js
+++ components/product-item.js
@@ -119,6 +119,7 @@ class ProductItem extends HTMLElement {
           align-self: center;
           justify-self: center;
           width: 100%;
+          max-height: 100%;
         }
         
         .title {
